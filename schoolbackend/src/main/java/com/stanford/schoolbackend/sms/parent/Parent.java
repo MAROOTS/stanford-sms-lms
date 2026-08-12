0e0a5871c2477a324dada8ac62e1e1520fac0c8f
MAROOTS <kademieugine@gmail.com>
2026-08-13 00:12:36 +0300

    parent student migration update

--- schoolbackend/src/main/java/com/stanford/schoolbackend/sms/parent/Parent.java
+++ schoolbackend/src/main/java/com/stanford/schoolbackend/sms/parent/Parent.java
@@ -1,13 +1,9 @@
 package com.stanford.schoolbackend.sms.parent;
 
 import com.stanford.schoolbackend.core.user.User;
-import com.stanford.schoolbackend.sms.student.Student;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Table(name = "parents")
